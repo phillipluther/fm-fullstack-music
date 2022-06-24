3ecfe9621b65fd4ba97d84ef4decc81849f44dc2
Phillip Luther <dev@phillipluther.com>
2022-06-24 05:23:55 -0700

    Protected routes and sign-in required

---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from './prisma';
+
+export const validateRoute = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const { TRAX_ACCESS_TOKEN: token } = req.cookies;
+
+    if (token) {
+      let user;
+
+      try {
+        const { id } = jwt.verify(token, 'secret');
+        user = await prisma.user.findUnique({
+          where: { id },
+        });
+
+        if (!user) {
+          throw new Error('User not found');
+        }
+      } catch (err) {
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
+      }
+
+      return handler(req, res, user);
+    }
+
+    res.status(401).json({ error: 'Unauthorized' });
+  };
+};
---
+++ pages/api/me.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { validateRoute } from '../../lib/auth';
+
+export default validateRoute((req: NextApiRequest, res: NextApiResponse, user) => {
+  res.json(user);
+});
